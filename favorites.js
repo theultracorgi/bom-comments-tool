0e37ce818db750e502b639b91d4758bfc1155f14
theultracorgi <kmathew@touchpadelectronics.com>
2021-12-22 15:34:11 -0600

    first commit

---
+++ favorites.js
@@ -0,0 +1,116 @@
+var timer = null; //timer for clearing the no cost inclusion field
+
+function onNoStockCopyClick(excludePricingReason, customer) { //returns note for no stock
+    if (hasValidInputs()) {
+        var output = getNotePrefix() + "No stock, component LT will not meet build req., "
+        if (excludePricingReason.value.length >= 3) { // checks if the price is included
+            output += excludePricingReason.value + ", no "
+
+            if (timer != null) { // timer to clear the Price not included field 
+                window.clearTimeout(timer);
+                timer = null;
+            }
+            timer = setTimeout(function () { excludePricingReason.value = ""; }, 3000);
+        }
+        output += "cost included in quote. Can " + customer.value + " supply or suggest alternates?" + appendOtherNotes('Fav');
+        navigator.clipboard.writeText(output);
+    }
+}
+
+function onEUStockCopyClick(euLeadTime) { // returns note for EU LT
+    if (hasValidInputs([euLeadTime])) {
+        navigator.clipboard.writeText(getNotePrefix() + "EU Stock with listed " + euLeadTime.value + "d transit time" + appendOtherNotes('Fav'));
+    } else {
+        document.getElementById('settings-button').click(); // focuses the settings menu which is actively highlighting an invalid EU LT field
+    }
+}
+
+function onPNNotFoundCopyClick() { // returns note for P/N not found
+    if (hasValidInputs()) {
+        navigator.clipboard.writeText(getNotePrefix() + "P/N not found, no cost included in quote. Please confirm P/N" + appendOtherNotes('Fav'));
+    }
+}
+
+function onQuotedAltClick(altMFRName, altMFRPN) { // returns note for alternate quoted
+    if (hasValidInputs([altMFRName, altMFRPN])) {
+        navigator.clipboard.writeText(getNotePrefix() + "No stock, quoted with alternate " + altMFRName.value + " " + altMFRPN.value + ", is this acceptable?" + appendOtherNotes('Fav'));
+    }
+}
+
+function onLowStockClick(lowStock) { // returns note for low stock
+    if (hasValidInputs([document.getElementById('lowStock')])) {
+        navigator.clipboard.writeText(getNotePrefix() + "Low stock (" + lowStock.value + ")" + appendOtherNotes('Fav'));
+    }
+}
+
+function onSpecMismatchClick(specDesc, listedSpec, bomDesc) { // returns note for spec mismatch
+    if (hasValidInputs([specDesc, listedSpec, bomDesc])) {
+        navigator.clipboard.writeText(getNotePrefix() + "Listed " + specDesc.value + " (" + listedSpec.value + ") does not match BOM desc (" + bomDesc.value + "), please confirm P/N" + appendOtherNotes('Fav'));
+    }
+}
+
+function onSourcingPartialClick(sourceableQTY, higherQTYLT) { // returns note for sourcing partial stock
+    if (hasValidInputs([sourceableQTY, higherQTYLT])) {
+        navigator.clipboard.writeText(getNotePrefix() + "Can source up to " + sourceableQTY.value + " QTY, higher QTYs subject to " + higherQTYLT.value + "w LT and potential volume MOQs" + appendOtherNotes('Fav'));
+    }
+}
+
+function onStockWithLeadTimeClick(leadTime) {
+    if (hasValidInputs([leadTime])) {
+        if (leadTime.value.includes('d') || leadTime.value.includes('w')) {
+            navigator.clipboard.writeText(getNotePrefix() + "Stock available with " + leadTime.value + " LT, is this acceptable? Cost included in quote" + appendOtherNotes('Fav'));
+        } else if (leadTime.value.includes('/') || leadTime.value.includes('-')) {
+            navigator.clipboard.writeText(getNotePrefix() + "Stock with expected ship date of " + leadTime.value + ", is this acceptable? Cost included in quote" + appendOtherNotes('Fav'));
+        } else {
+            navigator.clipboard.writeText(getNotePrefix() + "Stock available with " + leadTime.value + "w LT, is this acceptable? Cost included in quote" + appendOtherNotes('Fav'));
+        }
+    }
+}
+
+function onConfirmPNCorrectClick(mfrPN) {
+    if (hasValidInputs([mfrPN])) {
+        navigator.clipboard.writeText(getNotePrefix() + "Quoted with " + mfrPN.value + ", please confirm accurate" + appendOtherNotes('Fav'));
+    }
+}
+
+function onPackagingDiscrepancyClick(charDisc,oldPackage,newPackage) {
+    if (hasValidInputs([charDisc]) && ![oldPackage.selectedIndex,newPackage.selectedIndex].includes(0) && oldPackage.selectedIndex != newPackage.selectedIndex) {
+        navigator.clipboard.writeText(getNotePrefix() + "P/N discrepancy \"" + charDisc.value + "\" due to packaging (" + oldPackage.value + " vs " + newPackage.value + ")" + appendOtherNotes('Fav'));
+    }
+}
+
+function onQuotedAsSuppliedClick(reason, customer) {
+    if (hasValidInputs([reason])) {
+        navigator.clipboard.writeText(getNotePrefix() + reason.value + ", quoted as " + customer + " supplied, please confirm" + appendOtherNotes('Fav'));
+    }
+}
+
+function onQuotedWithInventoryClick(inventoryID) {
+    if (hasValidInputs([inventoryID])) {
+        navigator.clipboard.writeText(getNotePrefix() + "No distributor stock, quoted with TPE Inventory " + inventoryID.value + appendOtherNotes('Fav'));
+    }
+}
+
+function onAlternateApprovedClick(note) {
+    if (hasValidInputs([note])) {
+        navigator.clipboard.writeText(getNotePrefix() + note.value.substring(note.value.indexOf("with alternate ") + 15, note.value.indexOf(", is this")) + " approved" + appendOtherNotes("Fav"));
+    }
+}
+
+function onAlternateFromCustomerClick(alternate, customer) {
+    if (hasValidInputs([alternate])) {
+        navigator.clipboard.writeText(getNotePrefix() + alternate.value + " approved" + appendOtherNotes("Fav"));
+    }
+}
+
+function onCustomerToSupplyClick(customer) {
+    if (hasValidInputs()) {
+        navigator.clipboard.writeText(getNotePrefix() + customer + " to supply" + appendOtherNotes("Fav"));
+    }
+}
+
+function onOtherClick(otherNotes) { // returns the custom note
+    if (hasValidInputs([otherNotes])) {
+        navigator.clipboard.writeText(getNotePrefix() + otherNotes.value);
+    }
+}
---
+++ renderer.js
@@ -0,0 +1,296 @@
+//GLOBAL FUNCTIONS
+
+var allOptions = [ //List of all current widgets and associated tickers. THIS IS THE DB IT SHOULD PROBABLY BE A JSON BUT I DONT FEEL LIKE DOING THAT RIGHT NOW
+    ["<BLANK>", "dum"], 
+    ["No Stock", "nsk"],
+    ["EU Stock", "eus"],
+    ["P/N Not Found", "pnf"],
+    ["Quoted with Alternate", "qwa"],
+    ["Low Stock", "lst"],
+    ["Stock with Lead Time", "swl"],
+    ["Confirm P/N Correct", "cpc"],
+    ["Spec/BOM Desc Mismatch", "sbm"],
+    ["Packaging Discrepancy", "pkd"],
+    ["Quoted As Supplied", "qas"],
+    ["Quoted With Inventory", "qwi"],
+    ["Sourcing Partial", "srp"],
+    ["Alternate Approved", "caa"],
+    ["Alt From Customer", "afc"],
+    ["Customer Supplied", "cts"]
+    
+];
+
+var mkeToolPreset = [
+    ["<BLANK>", "dum"],                            //dummy doesn't need to be here but tbh it just helps with consistency
+    ["No Stock", "nsk"],
+    ["Low Stock", "lst"],
+    ["Quoted As Supplied", "qas"],
+    ["Quoted with Alternate", "qwa"],
+    ["Spec/BOM Desc Mismatch", "sbm"],
+    ["Packaging Discrepancy", "pkd"],
+    ["Confirm P/N Correct", "cpc"]
+];
+
+var custResponsePreset = [
+    ["<BLANK>", "dum"],
+    ["Quoted with Alternate", "qwa"],
+    ["Alternate Approved", "caa"],
+    ["Alt From Customer", "afc"],
+    ["Customer Supplied", "cts"],
+    ["EU Stock", "eus"],
+    ["P/N Not Found", "pnf"],
+    ["Stock with Lead Time", "swl"],
+    ["Confirm P/N Correct", "cpc"]
+];
+
+var numWidgetSlots = 8;
+
+if(numWidgetSlots >= allOptions.length) { // makes sure there aren't more placeholder slots than available widgets
+    numWidgetSlots = allOptions.length -1;
+}
+
+customLoadoutWidgetGenerator();
+
+const urlParams = new URLSearchParams(window.location.search);
+document.getElementById('initials').value = urlParams.get('user') || "";
+
+function onCustomerChange() { //literally just milwaukee tool cuz they special bois
+    if(['milwaukee tool', 'mke tool'].includes(event.target.value.toLowerCase())) {
+        document.getElementById('notesLoadout').selectedIndex = 1;
+        document.getElementById('notesLoadout').dispatchEvent(new Event('change'))
+    }
+}
+
+ //All my loops start at 1 and im not stupid its just the widgets all increment from 1 and it happened to work out that I needed a dummy element
+
+function onLoadoutPresetChange() {
+    for (var i = 1; i < numWidgetSlots +1; i++) {
+        if (document.getElementById(`widget${i}`).lastChild) {
+            document.getElementById('widgetHolder').appendChild(document.getElementById(`widget${i}`).lastChild); //only *direct* child is the widget div, so we just use lastChild
+        }
+    }
+    document.getElementById('custom-config').style.display = "none";
+
+    switch (event.target.value) {
+        case 'default':
+            for (var i = 1; i < numWidgetSlots +1; i++) { //again im not stupid its just the widgets index from 1 so thats why everything starts at 1
+                document.getElementById(`widget${i}`).appendChild(document.getElementById(allOptions[i][1]));
+            }
+            hideEmptyWidgets();
+            break;
+        case 'embedtek':
+            hideEmptyWidgets();
+            break;
+        case 'mketool':
+            for (var i = 1; i < mkeToolPreset.length; i++) { //no clue why i dont need to add 1 to the length, i think it has something to do with the dummy
+                document.getElementById(`widget${i}`).appendChild(document.getElementById(mkeToolPreset[i][1]));
+            }
+            hideEmptyWidgets();
+            break;
+        case 'custresponse':
+            for (var i = 1; i < custResponsePreset.length; i++) { //no clue why i dont need to add 1 to the length, i think it has something to do with the dummy
+                document.getElementById(`widget${i}`).appendChild(document.getElementById(custResponsePreset[i][1]));
+            }
+            hideEmptyWidgets();
+            break;
+        case 'custom':
+            document.getElementById('custom-config').style.display = "block";
+            for (var i = 1; i < numWidgetSlots +1; i++) {
+                var selection = document.getElementById(`loadoutSelection${i}`).value
+                if (selection !== "dum") {
+                    document.getElementById(`widget${i}`).appendChild(document.getElementById(selection));
+                }
+            }
+            hideEmptyWidgets();
+            break;
+    }
+}
+
+
+function onCustomLoadoutSelectionChange() {
+    for (var i = 1; i < numWidgetSlots +1; i++) { 
+        document.getElementById(`loadoutSelection${i}`).style.borderColor = "";
+        if (event.target.value === document.getElementById(`loadoutSelection${i}`).value && event.target.value !== 'dum' && i != event.target.name) {
+            document.getElementById(`loadoutSelection${i}`).selectedIndex = 0;
+            document.getElementById(`loadoutSelection${i}`).style.borderColor = "rgb(176, 0, 32)";
+        }
+    }
+    if (event.target.value !== 'dum') {
+        event.target.style.borderColor = "";
+        if (document.getElementById('widget' + event.target.name).lastChild) {
+            document.getElementById('widgetHolder').appendChild(document.getElementById('widget' + event.target.name).lastChild); //only *direct* child is the widget div, so we just use firstChild
+        }
+        document.getElementById('widget' + event.target.name).appendChild(document.getElementById(event.target.value));
+    }
+    hideEmptyWidgets();
+}
+
+function hideEmptyWidgets() { //hides widget slots that are unoccupied
+    for (var i = 1; i < numWidgetSlots +1; i++) { 
+        if (document.getElementById(`widget${i}`).firstChild) {
+            document.getElementById(`widget${i}`).style.display = "block";
+        } else {
+            document.getElementById(`widget${i}`).style.display = "none";
+        }
+    }
+
+}
+
+function onTabClick(navBarLabel, newString) { //updates nav bar heading when switching tabs
+    navBarLabel.innerHTML = newString;
+}
+function onEULeadTimeInput() { //updates EU LT heading to match input
+    document.getElementById("euLeadTimeLabel").innerHTML = "EU Stock with " + event.target.value + "d LT";
+}
+
+function clearActiveFields() {
+    document.querySelectorAll(['input','textarea']).forEach((input) => { //clears text inputs except initials, customer name, and EU LT
+        if (input.id === 'initials' || input.id === 'customer' || input.id === 'euLeadTime') {
+        } else {
+            input.value = '';
+        }
+    });
+
+    document.getElementById('stock-notice').querySelectorAll(['span','h2']).forEach((heading) => { //resets Stock Notice icons and headings
+        if (heading.id === 'copyAdvIcon') {
+        } else {
+            heading.style.color = 'black';
+        }
+    });
+
+    document.getElementById('stock-notice').querySelectorAll('h4').forEach((label) => { //resets Stock Notice labels
+        label.style.display = 'none';
+    });
+
+    document.getElementById('costOption1').checked = true;
+    document.getElementById('costOption1Parent').className = 'radio-item'; //resets cost option styles
+    document.getElementById('costOption2Parent').className = 'radio-item';
+    document.getElementById('costOption3Parent').className = 'radio-item';
+    document.getElementById('requiresApproval').checked = false;
+}
+
+function getNotePrefix() { // returns note prefix in format: 'YYYY-MM-DD FL:'
+    const initials = document.getElementById('initials');
+    var today = new Date();
+
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+
+    return today + " " + initials.value + ": "
+}
+
+function hasValidInputs(extraParamsArray) { // checks if customer and initials are filled out, returns boolean
+    document.querySelectorAll('input').forEach((input) => { //has to be called here so that when a non-parameter-requiring button is clicked it clears errors
+        if (input.id === 'initials' || input.id === 'customer') {
+        } else {
+            input.classList.remove('input-error'); //resets all fields first (prevents notes that were invalid at any point from being highlighted when not in use)
+        }
+    });
+    var returnValue = true;
+    if ([document.getElementById('initials').value, document.getElementById('customer').value].includes('')) {
+        document.getElementById('settings-button').click();
+        document.getElementById('customer').focus();
+        returnValue = false;
+    }
+    if(extraParamsArray) {
+        for (var i = 0; i < extraParamsArray.length; i++) {
+            if (extraParamsArray[i].value === '') {
+                extraParamsArray[i].classList.add('input-error');
+                returnValue = false;
+            }
+        }
+    }
+    
+    return returnValue;
+}
+
+
+function appendOtherNotes(tab) { //returns Other notes
+    if (document.getElementById('otherNotes' + tab).value !== '') {
+        return "\n\n" + document.getElementById('otherNotes' + tab).value;
+    } else {
+        return '';
+    }
+}
+
+
+function customLoadoutWidgetGenerator () {
+
+    var customConfigParent = document.getElementById('favorite-notes'); //creates the widget locations on the parent page
+
+    for (var j = 1; j < numWidgetSlots +1; j++) { // generates widgets for custom loadout
+        var widget = document.createElement('div');
+        widget.id = `widget${j}`;
+        widget.className = "favorite-note-container";
+        customConfigParent.appendChild(widget);
+        document.getElementById('widget' + j).appendChild(document.getElementById(allOptions[j][1])); //populates widget
+
+    }
+
+    customConfigParent.appendChild(document.getElementById('otr'));
+
+
+    //switches parent to be the selector parent on settings page
+    customConfigParent = document.getElementById('custom-config-wrapper'); // creates the selectors
+
+    for (var j = 1; j < numWidgetSlots +1; j++) { // generates selectors for custom loadout
+        var header = document.createElement('div');
+        header.className = 'note-header';
+    
+        var icon = document.createElement('span');
+        icon.className = 'material-icons-outlined';
+        icon.appendChild(document.createTextNode("filter_" + j));
+    
+        var select = document.createElement('select');
+        select.id = 'loadoutSelection' + j;
+        select.name = j;
+        select.setAttribute("onchange", "onCustomLoadoutSelectionChange()");
+    
+        header.appendChild(icon);
+        header.appendChild(select);
+        customConfigParent.appendChild(header);
+    
+        for (var i = 0; i < allOptions.length; i++) { //add list of widget options to custom loadout selectors
+            var opt = allOptions[i];
+            var el = document.createElement("option");
+            el.textContent = allOptions[i][0];
+            el.value = allOptions[i][1];
+            select.appendChild(el);
+        }
+        if(j < numWidgetSlots+1-4) {
+            select.selectedIndex = j;
+        } else {
+            select.selectedIndex = 0;
+        }
+        
+    }
+}
+
+function createRipple(event) {
+    const button = event.currentTarget;
+  
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+  
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+    circle.classList.add("ripple");
+  
+    const ripple = button.getElementsByClassName("ripple")[0];
+  
+    if (ripple) {
+      ripple.remove();
+    }
+  
+    button.appendChild(circle);
+  }
+  
+  const buttons = document.getElementsByTagName("button");
+  for (const button of buttons) {
+    button.addEventListener("click", createRipple);
+  }
